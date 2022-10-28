8d60aca70a7f7cd932b893ffb0dbde4a1272caa1
Clark Bynum <cbynum@clearblade.com>
2022-10-28 16:28:59 -0500

    more tests

--- src/tests/ClearBladeAsyncMock.test.ts
+++ src/tests/ClearBladeAsyncMock.test.ts
@@ -10,4 +10,25 @@ describe('ClearBladeAsyncMock', () => {
     expect(mock.mocks.collection.fetch).toHaveBeenCalled();
     expect(mock.mocks.query.equalTo).toHaveBeenCalled();
   });
+  it('handles mock overrides for collection', async () => {
+    const mock = new ClearBladeAsyncMock({
+      collection: {
+        fetch: jest.fn(() =>
+          Promise.resolve({
+            DATA: [
+              {
+                name: 'foo',
+              },
+            ],
+          }),
+        ),
+      },
+    });
+    const resp = await test(mock);
+    function test(client: CbServer.ClearBladeAsync) {
+      return client.Collection('test').fetch(client.Query().equalTo('name', 'ClearBlade'));
+    }
+
+    expect(resp).toEqual({ DATA: [{ name: 'foo' }] });
+  });
 });
